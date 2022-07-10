ccb8810504356b95d5c368e4764df38195db629f
unknown <jorge.pinzon@tecno.co>
2022-07-10 01:44:41 -0500

    Se ajusta carga del grafo, error en el path del archivo y en cantidad de la lista de adyacencia.

--- app/loadData.py
+++ app/loadData.py
@@ -1,29 +1,23 @@
 
-from asyncore import read
-from posixpath import split
-
-
 def load_graph():
-    G = [[]]
+    
+    G = [[],[],[],[],[]]
+    
     try:
         red = open("c:/project_algorimia/data/red.txt", "r")
         red_line = red.readline()
         vertices, calles = red_line.split()
-        indice_temp = 0
-        while red_line != '':    
+        #G = [list] * int(vertices)
+        count = 0
+
+        while count < int(calles):
             red_line = red.readline()
             indice, vertice_asociado, nom_calle, peso = red_line.split()
 
-            assoc = [int(vertice_asociado), int(peso), nom_calle]
-            print(assoc)
-            print("Indice a insertar", indice, "indice temp", indice_temp)
-
-            if indice == indice_temp:
-                G[int(indice)].extend(assoc)
-            else:
-                G[int(indice)].append(assoc)
-                
-            indice_temp = indice
+            assoc_vertex = (int(vertice_asociado), int(peso), nom_calle)
+            
+            G[int(indice)].append(assoc_vertex)
+            count = count + 1
                         
     except Exception as e: 
         print(repr(e))
--- app/main.py
+++ app/main.py
@@ -2,4 +2,3 @@ import loadData as load
 
 if __name__ == '__main__':
     print(load.load_graph())
-    
