b9b75ae4f29b39694742c946452a1c8ce93f0c9b
Ivan Barrios <ibarrios@trueblue.com>
2022-07-10 13:41:11 -0500

    Agregado lector del grafo

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from app.reader import Reader
+from reader import Reader
 
 _reader = Reader()
 
 
 if __name__ == '__main__':
-    #print(load.load_graph())
-    #print(load.load_graph())
-    print(_reader.read_journeys(r'../data/trayectos.txt'))
+    print(f"Grafo {_reader.read_graph(r'../data/red.txt')}")
+    print(f"Trayectos {_reader.read_journeys(r'../data/trayectos.txt')}")
 
--- app/reader.py
+++ app/reader.py
@@ -20,5 +20,33 @@ class Reader:
 
         return journeys
 
+    def read_graph(self, file):
+        i = 0
+        vertices = []
 
+        filepath = (self.directory / file).resolve()
+        with open(f'{filepath}', 'r') as f:
+            for line in f:
+                values = line.split()
+
+                if i == 0:
+                    vertices_number = int(values[0])
+
+                    for j in range(vertices_number):
+                        vertices.append([])
+                else:
+                    from_vertice = int(values[0])
+                    to_vertice = int(values[1])
+                    street_name = values[2]
+                    time_to_walk_it = int(values[3])
+
+                    adjacency = [to_vertice, street_name, time_to_walk_it]
+
+                    adjacencies = vertices[from_vertice]
+                    adjacencies.append(adjacency)
+                    vertices[from_vertice] = adjacencies
+
+                i = i + 1
+
+        return vertices
 
